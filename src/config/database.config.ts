5f320fad96bc100500db13870ce8c499c0172206
Sajad Sohrabi <sajjad.sohrabi2@gmail.com>
2023-06-29 17:54:27 +0330

    update env file

--- src/config/database.config.ts
+++ src/config/database.config.ts
@@ -1,10 +1,10 @@
 import { registerAs } from '@nestjs/config';
 
 export default registerAs('database', () => ({
-  host: process.env.database_host || 'localhost',
-  port: process.env.database_port || '5432',
-  username: process.env.database_username || 'postgres',
-  password: process.env.database_password || 'postgres',
-  name: process.env.database_name || 'logging',
-  type: process.env.database_type || 'postgres',
+  host: process.env.DATABASE_HOST || 'localhost',
+  port: process.env.DATABASE_PORT || '5432',
+  username: process.env.DATABASE_USERNAME || 'postgres',
+  password: process.env.DATABASE_PASSWORD || 'postgres',
+  name: process.env.DATABASE_NAME || 'logging',
+  type: process.env.DATABASE_TYPE || 'postgres',
 }));
--- src/config/logging.config.ts
+++ src/config/logging.config.ts
@@ -1,7 +1,7 @@
 import { registerAs } from '@nestjs/config';
 
 export default registerAs('logging', () => ({
-  enabled: process.env.logging_enabled === 'true' ? true : false,
-  save: process.env.logging_db_save === 'true' ? true : false,
-  consoleLog: process.env.logging_console === 'true' ? true : false,
+  enabled: process.env.LOGGING_ENABLED === 'true' ? true : false,
+  save: process.env.LOGGING_DB_SAVE === 'true' ? true : false,
+  consoleLog: process.env.LOGGING_CONSOLE === 'true' ? true : false,
 }));
